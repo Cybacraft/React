85546f1a1e0a18161803b773ed1e5441bf57092a
cybacraft <ukonudennis63@gmail.com>
2019-06-19 11:53:08 +0100

    reactClass1

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import TodoItems from './TodoItems'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TodoItems />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
