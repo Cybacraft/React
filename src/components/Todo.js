359f662b259005d4efdcd0bfd59186043bd9e0ca
cybacraft <ukonudennis63@gmail.com>
2019-06-28 12:25:20 +0100

    files

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,19 +4,40 @@ import TodoItems from './TodoItems'
 
 class Todo extends Component {
     state ={
-        todoItems: ['eat ' ,  'sleep ' ,  'train ' , 'fight ', 'rest','test'],
-        newItems:''
+        todoItems: [ ],
+        newTodo:''
     }
 
     handleChange =(e)=>{
-    this.setState ({newItems: e.target.value})
+    this.setState ({newTodo: e.target.value})
     }
 
     handleSubmit=(e)=>{
      e.preventDefault()
-     this.setState(
-         {todoItems: this.state.todoItems.concat(this.state.newItems)})
+     this.setState((prevState)=>{
+        return {
+            todoItems: prevState.todoItems.concat(this.state.newTodo),
+        newTodo: this.state.newTodo =''}})
     }
+
+    componentDidUpdate(prevPros, prevState){
+        if(prevState.todoItems.length !== this.state.todoItems.length){
+        const jsonState = JSON.stringify(this.state.todoItems)
+        localStorage.setItem('todoItems',jsonState)
+        }
+    }
+
+    componentDidMount(){
+        const itemsFromLocalStorage =localStorage.getItem('todoItems')
+        const todoItems =JSON.parse(itemsFromLocalStorage)
+        if(todoItems){
+            this.setState(() => ({
+                todoItems
+            }))
+        }
+            
+        }
+
 
         render(){
         return (
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Todo from './components/Todo';
-import Likes from './components/Likes'
+// import Likes from './components/Likes'
 import * as serviceWorker from './serviceWorker';
-import LocalStorage from './components/LocalStorage'
+// import LocalStorage from './components/LocalStorage'
 
 
-ReactDOM.render(<LocalStorage/>, document.getElementById('root'));
+ReactDOM.render(<Todo/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
