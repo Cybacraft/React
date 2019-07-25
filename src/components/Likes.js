08b6f474c69083691a4b0d6622fa30e2e5541cdd
cybacraft <ukonudennis63@gmail.com>
2019-07-25 21:33:44 +0100

    todoApp

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import Header from './Header';
+import likesStyles from '../Styles/likes.module.css'
 
 class Likes extends React.Component{
  state = {
@@ -33,13 +34,15 @@ resetsLikes = () => { this. setState((prevState)=>{
 
  render(){
   return(
-   <div>
-     <Header Title = 'My title header'/>
+   <div className={likesStyles.wrapper}>
+     <Header title = "LIKES APP"/>
     <h1>Welcome to my likes app</h1>
     <h3>likes: {this.state.likes}</h3>
-    <button onClick ={this.increaseLikes}>like</button>
-    <button onClick ={this.decreaseLikes}>unlike</button>
-    <button onClick ={this.resetsLikes}>reset</button>
+    <div className={likesStyles.buttonWrapper}>
+    <button className={likesStyles.button}onClick ={this.increaseLikes}>like</button>
+    <button className={likesStyles.button}onClick ={this.decreaseLikes}>unlike</button>
+    <button className={likesStyles.button}onClick ={this.resetsLikes}>reset</button>
+   </div>
    </div>
   )
  }
--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react'
+import todoItemsStyle from '../Styles/todoItems.module.css'
 
-const TodoItems = (props) =>{
+const TodoItems = props =>{
    // destructuring props
    const { individualItem, handleRemoveOneItem} = props
     return (
-        <div>
+        <div className={todoItemsStyle.todoItems}>
             {individualItem}
-            <button onClick={ e => {
+            <button className={todoItemsStyle.removeButton} onClick={ e => {
             handleRemoveOneItem(individualItem)
         }}>Remove</button> 
         </div>
